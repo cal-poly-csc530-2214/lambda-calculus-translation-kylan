14bf57196feb66ac2f36e921bebc087fed0ea508
Kylan Stewart <kylan.stewart@gmail.com>
2021-04-11 10:09:59 -0700

    Very basic implementation working

--- LCTranslation.py
+++ LCTranslation.py
@@ -22,14 +22,46 @@ def tokenize(i):
     return parsed
 
 def identify(parsed):
+    # LC = num -> "<num>"
+    if type(parsed) == int:
+        return str(parsed)
+    # LC = id -> "<id>"
+    elif type(parsed) == sexpdata.Symbol:
+        return sexpdata.dumps(parsed)
+
     head = parsed[0]
-    if type(head) == sexpdata.Symbol:
-        print(head)
+    print(parsed)
+    # LC = (/ id => LC) -> "lambda <id> : LC"
+    if head == sexpdata.Symbol('/'):
+        return "lambda " + identify(parsed[1]) + ": " + identify(parsed[3])
+    # LC = (+ LC LC) -> "(LC1 + LC2)"
+    elif head == sexpdata.Symbol('+'):
+        return "(" + identify(parsed[1]) + " + " + identify(parsed[2]) + ")"
+    # LC = (* LC LC) -> "(LC1 * LC2)"
+    elif head == sexpdata.Symbol('*'):
+        return "(" + identify(parsed[1]) + " * " + identify(parsed[2]) + ")"
+    # LC = (ifleq0 LC LC LC) -> 
+    # if (LC1 <= 0):
+    #    LC2
+    # else:
+    #    LC3
+    elif head == sexpdata.Symbol('ifleq0'):
+        cond = identify(parsed[1])
+        c1 = identify(parsed[2])
+        c2 = identify(parsed[3])
+        return "if (" + cond + " <= 0):\n   " + c1 + "\nelse:\n   " + c2
+    # LC = (println LC) -> "print(LC)"
+    elif head == sexpdata.Symbol('println'):
+        return "print(" + identify(parsed[1]) + ")"
+    # LC = (LC LC) -> "(LC1(LC2))"
+    else:
+        return "(" + identify(head) + "(" + identify(parsed[1]) + "))"
 
 def main():
     for i in testfiles:
         print("========== " + i + " ========")
         parsed = tokenize(i)
-        print(parsed)
+        returned = identify(parsed)
+        print(returned)
 
 main()
